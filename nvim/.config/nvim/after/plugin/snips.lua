ae17a11de5099674da720ac2a227e6048f34c66f
Tobias Wyss <towyss@gmail.com>
2023-05-07 12:53:22 +0200

    update dotfiles

---
+++ nvim/.config/nvim/after/plugin/snips.lua
@@ -0,0 +1,11 @@
+vim.cmd( [[ 
+  call nvimhs#start('/Users/wysstobi/workspaces/fhnw/active/fprod/snips-nvim', 'snips', []) 
+]])
+
+vim.keymap.set("n", "<F5>",
+  function()
+    vim.cmd([[ call nvimhs#compileAndRestart('snips') ]])
+    print("recompiling...")
+  end
+)
+
