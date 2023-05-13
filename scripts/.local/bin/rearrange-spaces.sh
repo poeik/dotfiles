ae622b072e46d1943dc499a755f1217bd6287ffb
Tobias Wyss <towyss@gmail.com>
2023-05-13 11:31:04 +0200

    update rearrange-spaces script

--- scripts/.local/bin/rearrange-spaces.sh
+++ scripts/.local/bin/rearrange-spaces.sh
@@ -19,27 +19,26 @@ apps["Microsoft Outlook"]=8
 apps["Microsoft Teams"]=7
 
 start () {
-  displayAmount=$(yabai -m query --displays | jq -c "length")
+  local displayAmount=$(yabai -m query --displays | jq -c "length")
   if [[ ! ($displayAmount -eq 3 || $displayAmount -eq 1) ]]; then 
     echo "Behaviour for $displayAmount is not defined, exit"
     return 0
   fi
   echo "Currently are $displayAmount display attached."
 
-  spacesPerDisplay=$((desiredSpaceAmount / displayAmount))
+  local spacesPerDisplay=$((desiredSpaceAmount / displayAmount))
 
   for i in {1..$displayAmount}
   do
-    focusDisplay $i
-    currentSpaceAmountOfActiveDisplay=$(yabai -m query --displays --display | jq ".spaces | length")
-    diff=$((currentSpaceAmountOfActiveDisplay - spacesPerDisplay))
+    local displayId=$i
+    focusDisplay $displayId
+    local currentSpaceAmountOfActiveDisplay=$(yabai -m query --displays --display | jq ".spaces | length")
+    local diff=$((currentSpaceAmountOfActiveDisplay - spacesPerDisplay))
 
     if (( diff > 0 )); then
-      echo "too many spaces, destroy! $diff"
       destroySpaces $diff $displayId
     elif (( diff < 0)); then
-      echo "too few spaces create! ${diff#-}"
-      createSpaces ${diff#-} # with absolute value
+      createSpaces ${diff#-} $displayId # with absolute value
     fi
   done
 
@@ -48,7 +47,9 @@ start () {
 }
 
 createSpaces() {
-  amount=$1
+  local amount=$1
+  local displayId=$2
+  echo "display $displayId has too few spacs on it - $amount spaces will be created!" 
   for i in {1..$amount}
   do
     yabai -m space --create
@@ -56,31 +57,31 @@ createSpaces() {
 }
 
 destroySpaces() {
-  amount=$1
-  displayId=$2
-  spacesOnCurrentDisplay=($(yabai -m query --displays --display $displayId | jq ".spaces" | sed -e 's/\[//g' -e 's/\]//g' -e 's/\,/ /g'))
-  amountOfSpacesOnCurrentDisplay=${#spacesOnCurrentDisplay[@]}
-  destroyTo=$((amountOfSpacesOnCurrentDisplay - amount + 1))
+  local amount=$1
+  local displayId=$2
+  local spacesOnCurrentDisplay=($(yabai -m query --displays --display $displayId | jq ".spaces" | sed -e 's/\[//g' -e 's/\]//g' -e 's/\,/ /g'))
+  local amountOfSpacesOnCurrentDisplay=${#spacesOnCurrentDisplay[@]}
+  local destroyTo=$((amountOfSpacesOnCurrentDisplay - amount + 1))
   echo "display $displayId has following spaces on it: $spacesOnCurrentDisplay, the last $amount will be destroyed!"
   for i in {$amountOfSpacesOnCurrentDisplay..$destroyTo}
   do
-    nextSpaceToDestroy=$spacesOnCurrentDisplay[$i]
+    local nextSpaceToDestroy=$spacesOnCurrentDisplay[$i]
     echo "destroy space: $nextSpaceToDestroy on display $displayId"
     yabai -m space --destroy $nextSpaceToDestroy
   done
 }
 
 focusDisplay() {
-  displayId=$1
+  local displayId=$1
   yabai -m display --focus $displayId
 }
 
 moveWindowsToCorrectSpaces() {
   # Assoziatives Array mit Namen und Space
   for key space in ${(kv)apps}; do
-    idsAsString=$(yabai -m query --windows | jq -c ".[] | select(.app | contains($key)) | .id")
-    idsAsString="${idsAsString//[^0-9]/\n}" 
-    window_ids=("${(@s/\n/)idsAsString}") #" # this hash is only because of lsp problems
+   local idsAsString=$(yabai -m query --windows | jq -c ".[] | select(.app | contains($key)) | .id")
+   local idsAsString="${idsAsString//[^0-9]/\n}" 
+   local window_ids=("${(@s/\n/)idsAsString}") #" # this hash is only because of lsp problems
 
     # echo "$key $window_ids"
     for window_id in "${window_ids[@]}" 
