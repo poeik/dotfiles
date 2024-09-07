907ede49af54b1251ec35f59075416d0fd57720b
Tobias Wyss <towyss@gmail.com>
2024-09-07 14:55:24 +0200

    use ctrl+w hjkl for nvim/tmux pane navigation and ctrl+hjkl for window navigation

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,9 +1,10 @@
 return {
   'christoomey/vim-tmux-navigator',
   config = function()
-    vim.keymap.set("n","<C-h>", vim.cmd.TmuxNavigateLeft, { silent = true })
-    vim.keymap.set("n","<C-j>", vim.cmd.TmuxNavigateDown, { silent = true })
-    vim.keymap.set("n","<C-k>", vim.cmd.TmuxNavigateUp, { silent = true })
-    vim.keymap.set("n","<C-l>", vim.cmd.TmuxNavigateRight, { silent = true })
+    vim.keymap.set("n","<C-w>h", vim.cmd.TmuxNavigateLeft, { silent = true })
+    vim.keymap.set("n","<C-w>j", vim.cmd.TmuxNavigateDown, { silent = true })
+    vim.keymap.set("n","<C-w>k", vim.cmd.TmuxNavigateUp, { silent = true })
+    vim.keymap.set("n","<C-w>l", vim.cmd.TmuxNavigateRight, { silent = true })
+    vim.keymap.set("n","<C-w>w", vim.cmd.TmuxNavigatePrevious, { silent = true })
   end
 }
