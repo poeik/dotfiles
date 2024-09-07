c832e73005aa5b23cef75b614143357c2c602183
Tobias Wyss <towyss@gmail.com>
2024-09-07 15:24:07 +0200

    annotate with git blame using fugitive on <leader>ga

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,19 @@
 return {
   'tpope/vim-fugitive',
-  config = function() 
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- open git window within nvim
+  config = function()
+    vim.keymap.set("n", "<leader>tg", vim.cmd.Git)
+    vim.keymap.set("n", "<leader>ga", function()
+      local buffers = vim.api.nvim_list_bufs()
+      local fugitive_buffers = vim.tbl_filter(function(buffer)
+        return vim.api.nvim_buf_is_loaded(buffer) and vim.bo[buffer].filetype == 'fugitiveblame'
+      end, buffers)
+
+      if #fugitive_buffers > 0 then
+        vim.api.nvim_buf_delete(fugitive_buffers[1], { force = true })
+      else
+        vim.cmd('G blame')
+      end
+    end)
   end
 
 }
