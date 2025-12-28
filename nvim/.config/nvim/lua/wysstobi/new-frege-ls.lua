fde19c473da1722739700a8a4fae4ec70b43b2fd
tobias.wyss <tobias.wyss@unic.com>
2025-12-28 21:30:47 +0100

    Get rid off Frege nightly

--- nvim/.config/nvim/lua/wysstobi/new-frege-ls.lua
+++ nvim/.config/nvim/lua/wysstobi/new-frege-ls.lua
@@ -1,44 +1,16 @@
 local lspconfig = require("lspconfig")
--- load Frege nightly ls for test projects
--- load new Frege stable ls for all other projects
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-
-    local cwd = vim.fn.getcwd()
-    local nightly_paths = {
-      vim.fn.expand("~") .. "/workspaces/mse/project8/TestFregeLS",
-      -- extend as we go
-    }
-    if vim.tbl_contains(nightly_paths, cwd) then
-      ConfigureFregeNightly()
-    else
-      ConfigureFregeStable()
-    end
+    ConfigureFregeStable()
   end,
 })
 
-ConfigureFregeNightly = function()
-  print("frege-ls-nighly loaded")
-
-  require('lspconfig.configs').new_frege_nightly = {
-    default_config = {
-      cmd = { "java", "-Xss4m", "-jar", vim.fn.expand("~") .. "/workspaces/mse/project8/frege-lsp/build/libs/frege-lsp.jar", vim.fn.expand("~") .. "/workspaces/mse/project8/frege-lsp/log.txt" },
-      name = "new Frege LS (nightly)",
-      filetypes = { 'frege' },
-      root_dir = lspconfig.util.root_pattern("build.gradle", "Makefile"),
-      settings = {},
-    }
-  }
-
-  lspconfig.new_frege_nightly.setup {}
-end
-
 ConfigureFregeStable = function()
-  print("frege-ls loaded")
-
   require('lspconfig.configs').new_frege_ls = {
     default_config = {
-      cmd = { "java", "-Xss4m", "-jar", vim.fn.expand("~") .. "/workspaces/mse/frege/utils/new-frege-lsp-stable/frege-lsp.jar", vim.fn.expand("~") .. "/workspaces/mse/frege/utils/new-frege-lsp-stable/log.txt"},
+      -- cmd = { "java", "-Xss1024m", "-jar", vim.fn.expand("~") .. "/workspaces/mse/frege/utils/new-frege-lsp-stable/frege-lsp.jar", vim.fn.expand("~") .. "/workspaces/mse/frege/utils/new-frege-lsp-stable/log.txt"},
+      cmd = { "sh", vim.fn.expand("~") .. "/workspaces/mse/project8/frege-ls/build/install/frege-ls/bin/frege-ls" },
       name = "new Frege LS",
       filetypes = { 'frege' },
       root_dir = lspconfig.util.root_pattern("build.gradle", "Makefile"),
@@ -46,5 +18,19 @@ ConfigureFregeStable = function()
     }
   }
 
-  lspconfig.new_frege_ls.setup {}
+  lspconfig.new_frege_ls.setup {
+    on_attach = function(client, bufnr)
+        vim.keymap.set("n", "<leader>rr", function()
+          vim.cmd("! gradle compileFrege && gradle installDist")
+        end, { buffer = bufnr, silent = true })
+
+        vim.keymap.set("n", "<leader>rb", function()
+          vim.cmd("! gradle compileFrege")
+        end, { buffer = bufnr, silent = true })
+
+        vim.keymap.set("n", "<leader>rt", function()
+          vim.cmd("! gradle testFrege")
+        end, { buffer = bufnr, silent = true })
+      end
+  }
 end
