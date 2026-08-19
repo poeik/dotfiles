732e204ee9cc7878f5f682cf9c541dedf4d5f249
tobias.wyss <tobias.wyss@unic.com>
2026-08-19 14:57:03 +0200

    Use the md plugin for the codecompanion buffer

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -7,6 +7,7 @@ return {
     config = function ()
       require('render-markdown').setup({
         completions = { lsp = { enabled = false } },
+        file_types = { 'markdown', 'codecompanion' },
       })
     end
 }
